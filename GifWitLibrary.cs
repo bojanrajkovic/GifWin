239098825cada63d22401d322b7c4aeaa3de5e35
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2014-12-02 19:31:28 -0500

    Gifs + Windows = Gifwin.

---
+++ GifWitLibrary.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace GifWin
+{
+    class GifWitLibrary : IEnumerable<GifWitLibraryEntry>
+    {
+        public IEnumerator<GifWitLibraryEntry> GetEnumerator()
+        {
+            return Images.AsReadOnly().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public int Version { get; set; }
+        internal List<GifWitLibraryEntry> Images { get; set; }
+    }
+
+    internal class GifWitLibraryEntry
+    {
+        public string Url { get; set; }
+        public string[] Keywords { get; set; }
+    }
+}
