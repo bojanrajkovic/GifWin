6a96fa1f1d07056feb1ab2e03c009953031964b5
Eric Maupin <me@ermau.com>
2014-12-29 16:01:23 -0500

    Cache gifs locally and move to entry VM

--- src/GifWin/GifWitLibrary.cs
+++ src/GifWin/GifWitLibrary.cs
@@ -1,7 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
 using Newtonsoft.Json;
 
 namespace GifWin
@@ -30,6 +34,39 @@ namespace GifWin
                 string content = await reader.ReadToEndAsync().ConfigureAwait (false);
                 return JsonConvert.DeserializeObject<GifWitLibrary> (content);
             }
+        }
+
+        /// <returns>Path to the local file</returns>
+        internal static Task<string> GetOrMakeSavedAsync (GifWitLibraryEntry entry)
+        {
+            return Task.Run (async () => {
+                SHA1 sha1 = SHA1.Create();
+                byte[] hash = sha1.ComputeHash (Encoding.Unicode.GetBytes (entry.Url.ToString()));
+                string readableHash = GetReadableHash (hash);
+
+                DirectoryInfo storage = new DirectoryInfo (Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.LocalApplicationData), "GifWin", "Cache"));
+                storage.Create();
+
+                FileInfo file = new FileInfo (Path.Combine (storage.FullName, readableHash + ".gif"));
+                if (!file.Exists) {
+                    var client = new WebClient();
+                    byte[] contents = await client.DownloadDataTaskAsync (entry.Url).ConfigureAwait (false);
+                    using (FileStream stream = file.OpenWrite()) {
+                        await stream.WriteAsync (contents, 0, contents.Length).ConfigureAwait (false);
+                    }
+                }
+
+                return file.FullName;
+            });
+        }
+
+        private static string GetReadableHash (byte[] hash)
+        {
+            StringBuilder builder = new StringBuilder (hash.Length * 2);
+            for (int i = 0; i < hash.Length; i++)
+                builder.Append (hash[i].ToString ("X2"));
+
+            return builder.ToString();
         }
     }
 }
--- src/GifWin/GifWitLibraryEntry.cs
+++ src/GifWin/GifWitLibraryEntry.cs
@@ -1,22 +1,23 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace GifWin
 {
     internal class GifWitLibraryEntry
     {
-        [JsonProperty("url")]
-        public Uri Url { get; set; }
+        private Uri url;
+
+        [JsonProperty ("url")]
+        public Uri Url
+        {
+            get;
+            set;
+        }
 
         // Ugh this is dumb.
         [JsonProperty("keywords")]
         public string KeywordString { get; set; }
-
-        [JsonIgnore]
-        public string[] Keywords
-        {
-            get { return KeywordString.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries); }
-            set { KeywordString = string.Join(" ", value); }
-        }
     }
 }
