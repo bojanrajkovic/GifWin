3a39b4c9fb2eb0917ab5b4771e0120c3d24e0ae9
Eric Maupin <me@ermau.com>
2014-12-29 00:21:58 -0500

    MVVMify some stuff
    - Asyncifies loading the library
    - Uses built-in mechanisms for filtering the list
    - Uses the binding system to simply do the search delays

--- src/GifWin/GifWitLibrary.cs
+++ src/GifWin/GifWitLibrary.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace GifWin
@@ -23,9 +24,12 @@ namespace GifWin
         [JsonProperty("images")]
         internal List<GifWitLibraryEntry> Images { get; set; }
 
-        internal static GifWitLibrary LoadFromFile(string path)
+        internal static async Task<GifWitLibrary> LoadFromFileAsync (string path)
         {
-            return JsonConvert.DeserializeObject<GifWitLibrary>(File.ReadAllText(path));
+            using (StreamReader reader = new StreamReader (File.Open (path, FileMode.Open))) {
+                string content = await reader.ReadToEndAsync().ConfigureAwait (false);
+                return JsonConvert.DeserializeObject<GifWitLibrary> (content);
+            }
         }
     }
 }
---
+++ src/GifWin/ViewModelBase.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace GifWin
+{
+    internal abstract class ViewModelBase
+        : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged ([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+                handler (this, new PropertyChangedEventArgs (propertyName));
+        }
+    }
+}
