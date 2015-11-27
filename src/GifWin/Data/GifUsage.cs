b1bbac51179f1af779ba0416b1723550cc94baa6
Bojan Rajkovic <bojan.rajkovic@xamarin.com>
2015-11-26 22:11:21 -0500

    Add DB storage for usage data.

---
+++ src/GifWin/Data/GifUsage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GifWin.Data
+{
+    [Table("usages")]
+    public class GifUsage
+    {
+        public int Id { get; set; }
+        public DateTimeOffset UsedAt { get; set; }
+        public virtual GifEntry Gif { get; set; }
+    }
+}
---
+++ src/GifWin/Migrations/20151127030513_UsageDataStorage.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Data.Entity.Migrations;
+
+namespace GifWin.Migrations
+{
+    public partial class UsageDataStorage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "usages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    GifId = table.Column<int>(nullable: true),
+                    UsedAt = table.Column<DateTimeOffset>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GifUsage", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GifUsage_GifEntry_GifId",
+                        column: x => x.GifId,
+                        principalTable: "gifs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable("usages");
+        }
+    }
+}
