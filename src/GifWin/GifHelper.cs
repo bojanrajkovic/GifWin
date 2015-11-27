a2e6a69cc618b81b26e1822908c1708d34fb16e4
Bojan Rajkovic <bojan.rajkovic@xamarin.com>
2015-11-26 21:21:14 -0500

    Move helper methods to GifHelper class.

---
+++ src/GifWin/GifHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GifWin
+{
+    class GifHelper
+    {
+        /// <returns>Path to the local file</returns>
+        internal static Task<string> GetOrMakeSavedAsync(string entryUrl)
+        {
+            return Task.Run(async () => {
+                SHA1 sha1 = SHA1.Create();
+                byte[] hash = sha1.ComputeHash(Encoding.Unicode.GetBytes(entryUrl));
+                string readableHash = GetReadableHash(hash);
+
+                DirectoryInfo storage = new DirectoryInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "GifWin", "Cache"));
+                storage.Create();
+
+                FileInfo file = new FileInfo(Path.Combine(storage.FullName, readableHash + ".gif"));
+                if (!file.Exists) {
+                    var client = new WebClient();
+                    byte[] contents = await client.DownloadDataTaskAsync(entryUrl).ConfigureAwait(false);
+                    using (FileStream stream = file.OpenWrite()) {
+                        await stream.WriteAsync(contents, 0, contents.Length).ConfigureAwait(false);
+                    }
+                }
+
+                return file.FullName;
+            });
+        }
+
+        private static string GetReadableHash(byte[] hash)
+        {
+            StringBuilder builder = new StringBuilder(hash.Length * 2);
+            for (int i = 0; i < hash.Length; i++)
+                builder.Append(hash[i].ToString("X2"));
+
+            return builder.ToString();
+        }
+    }
+}
--- src/GifWin/GifWitLibrary.cs
+++ src/GifWin/GifWitLibrary.cs
@@ -35,38 +35,5 @@ namespace GifWin
                 return JsonConvert.DeserializeObject<GifWitLibrary> (content);
             }
         }
-
-        /// <returns>Path to the local file</returns>
-        internal static Task<string> GetOrMakeSavedAsync (GifWitLibraryEntry entry)
-        {
-            return Task.Run (async () => {
-                SHA1 sha1 = SHA1.Create();
-                byte[] hash = sha1.ComputeHash (Encoding.Unicode.GetBytes (entry.Url.ToString()));
-                string readableHash = GetReadableHash (hash);
-
-                DirectoryInfo storage = new DirectoryInfo (Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.LocalApplicationData), "GifWin", "Cache"));
-                storage.Create();
-
-                FileInfo file = new FileInfo (Path.Combine (storage.FullName, readableHash + ".gif"));
-                if (!file.Exists) {
-                    var client = new WebClient();
-                    byte[] contents = await client.DownloadDataTaskAsync (entry.Url).ConfigureAwait (false);
-                    using (FileStream stream = file.OpenWrite()) {
-                        await stream.WriteAsync (contents, 0, contents.Length).ConfigureAwait (false);
-                    }
-                }
-
-                return file.FullName;
-            });
-        }
-
-        private static string GetReadableHash (byte[] hash)
-        {
-            StringBuilder builder = new StringBuilder (hash.Length * 2);
-            for (int i = 0; i < hash.Length; i++)
-                builder.Append (hash[i].ToString ("X2"));
-
-            return builder.ToString();
-        }
     }
 }
