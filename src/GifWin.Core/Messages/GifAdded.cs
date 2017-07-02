401e4223f982d95d380e9f20828a9fb3e2eb5007
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2017-07-02 01:07:01 -0400

    GifWin: Add missing files.

---
+++ src/GifWin.Core/Messages/GifAdded.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.Annotations;
+
+using GifWin.Core.Models;
+
+namespace GifWin.Core.Messages
+{
+    [PublicAPI]
+    public sealed class GifAdded
+    {
+        public GifEntry NewGif { get; set; }
+    }
+}
---
+++ src/GifWin.Core/Services/MessagingService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+using JetBrains.Annotations;
+
+namespace GifWin.Core.Services
+{
+    [PublicAPI]
+    public static class MessagingService
+    {
+        static readonly Dictionary<Type, object> Subscribers = new Dictionary<Type, object>();
+
+        delegate bool ProcessMessage<in T>(T messageType);
+
+        class Subscription<T> : IDisposable
+        {
+            public Subscription(Func<T, bool> handler) =>
+                Handler = new ProcessMessage<T>(handler);
+
+            public ProcessMessage<T> Handler { get; }
+
+            public void Dispose()
+            {
+                var subs = (LinkedList<Subscription<T>>)Subscribers[typeof(T)];
+                subs.Remove(this);
+            }
+        }
+
+        public static void Send<T>(T message)
+        {
+            var messageType = typeof(T);
+
+            if (Subscribers.TryGetValue(messageType, out var untypedSubscribers)) {
+                var typedSubscribers = (LinkedList<Subscription<T>>)untypedSubscribers;
+                foreach (var typedSubscriber in typedSubscribers) {
+                    var result = typedSubscriber.Handler(message);
+
+                    // If the handler returns `true`, that means we should stop processing this
+                    // message.
+                    if (result)
+                        break;
+                }
+            }
+        }
+
+        public static IDisposable Subscribe<T>(Func<T, bool> handler)
+        {
+            var messageType = typeof(T);
+            var sub = new Subscription<T>(handler);
+
+            if (Subscribers.TryGetValue(messageType, out var untypedSubscribers)) {
+                var typedSubscribers = (LinkedList<Subscription<T>>)untypedSubscribers;
+                typedSubscribers.AddLast(sub);
+            } else {
+                var subscribersList = new LinkedList<Subscription<T>>();
+                subscribersList.AddLast(sub);
+                Subscribers[messageType] = subscribersList;
+            }
+
+            return sub;
+        }
+    }
+}
